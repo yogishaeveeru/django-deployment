9a2b89832058552242621d68ea44d5c71d3aea64
Yogishaeveeru <yogishaeveeru@gmail.com>
2022-10-24 13:53:32 +0530

    db changed

--- myworld/myworld/settings.py
+++ myworld/myworld/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'myworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydatabase',
-        'USER':'Veeru',
-        'PASSWORD':'Veeru@123*',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
